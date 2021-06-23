311a40f1e7aac605b86ba35171f8bb1cfb82b5ad
abhinav1015 <maharjanabhinav0@gmail.com>
2021-06-23 08:07:40 +0545

    Insertion Sort

---
+++ InsertionSort.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class InsertionSort {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an array: ");
+        int n = sc.nextInt();
+        int[] numbers = new int[n];
+        System.out.println("Enter the elements in the array");
+        for(int i=0;i<n;i++){
+            numbers[i] = sc.nextInt();
+        }
+
+        for(int i=1;i<n;i++){
+            int current = numbers[i];
+            int j = i-1;
+            while(j>=0 && numbers[j]>current  ){
+                numbers[j+1]=numbers[j];
+                j--;
+            }
+            numbers[j+1] = current;
+        }
+        System.out.println("The sorted array is: ");
+        for(int i=0;i<n;i++){
+            System.out.print(numbers[i] + "   \n");
+        }
+    }
+}
